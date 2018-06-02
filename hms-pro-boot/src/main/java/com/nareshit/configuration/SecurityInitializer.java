5ed421d7160e8429e7f6800cff77feade30bbaa4
Narsi <narsi.sanda@gmail.com>
2018-06-02 20:34:12 +0530

    deleted

--- hms-pro-boot/src/main/java/com/nareshit/configuration/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.nareshit.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-  public SecurityInitializer(){
-	  super(WebSecurityConfigurer.class);
-  }
-}
